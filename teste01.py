54bde6d92137f256b9b600efbdaf687d7a7506fa
LeTec-net <leticiasss185@gmail.com>
2026-05-29 16:49:49 -0300

    ok

--- teste01.py
+++ teste01.py
@@ -1,7 +1,13 @@
-name = input("Digite seu nome: ")
+nome = input("Digite seu nome: ")
 
 # Concatenando
 print("Olá " + nome)
 
 print(f"olá novamente {nome}!")
 # Texto formatado → Interpolar
+
+idade = input("Qual sua idade? ")
+
+idade = int(idade)
+
+print(idade + 10)
